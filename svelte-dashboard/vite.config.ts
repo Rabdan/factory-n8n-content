6b2b9b725ec0d7f11284a5ae4ca922027494f904
Rabdan <rabdan@itonly.ru>
2026-01-16 16:45:11 +0700

    config

--- svelte-dashboard/vite.config.ts
+++ svelte-dashboard/vite.config.ts
@@ -1,21 +1,17 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		port: 5175,
-		strictPort: true,
-		host: true,
-		proxy: {
-			'/api': {
-				target: process.env.VITE_API_URL || 'http://localhost:3001',
-				changeOrigin: true,
-			},
-			'/uploads': {
-				target: process.env.VITE_API_URL || 'http://localhost:3001',
-				changeOrigin: true,
-			}
-		}
-	}
+  plugins: [sveltekit()],
+  server: {
+    port: 5175,
+    strictPort: true,
+    host: true,
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_URL || "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
 });
